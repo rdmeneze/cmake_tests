3877a8f71d4483ea3db7aade6f17ca030aa2ae9c
Rafael Dias <rdmeneze@gmail.com>
2023-07-14 11:28:38 +0200

    test

--- getting_start/main.cpp
+++ getting_start/main.cpp
@@ -1,11 +1,10 @@
-#include <iostream>
 #include "CMain.h"
+#include <iostream>
 
-int main(int argc, char **argv) 
-{
-	CMain* app = new CMain(argc, argv);
+int main(int argc, char **argv) {
+  CMain *app = new CMain(argc, argv);
 
-	std::cout << "Version: " << app->version() << "\n";
+  std::cout << "Version: " << app->version() << "\n";
 
-	return app->execute();
+  return app->execute();
 }
